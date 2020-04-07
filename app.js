813bfe33296170a444b695bb84868eef199fbac7
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2020-04-07 16:34:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,65 +5,8 @@ const passport = require('./utils/pass');
 const app = express();
 const port = 3000;
 
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
-
 const loggedIn = (req, res, next) => {
-  if (req.user) {
-    next();
-  } else {
-    res.redirect('/form');
-  }
-};
-
-app.use(session({
-  secret: 'salainen kissa',
-  resave: false,
-  saveUninitialized: true,
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.set('views', './views');
-app.set('view engine', 'pug');
-
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/setCookie/:clr', (req, res) => {
-  res.cookie('color', req.params.clr).send('cookie set');
-});
-
-app.get('/deleteCookie', (req, res) => {
-  res.clearCookie('color');
-  res.send('cookie color cleared');
-});
-
-app.get('/form', (req, res) => {
-  res.render('form');
-});
-
-app.get('/secret', loggedIn, (req, res) => {
-  res.render('secret');
-});
-
-app.post('/login',
-    passport.authenticate('local', {failureRedirect: '/form'}),
-    (req, res) => {
-      console.log('success');
-      res.redirect('/secret');
-    });
-
-'use strict';
-const express = require('express');
-const session = require('express-session');
-const passport = require('./utils/pass');
-const app = express();
-const port = 3000;
-
-const loggedIn = (req, res, next) => {
+  console.log('logged', req.user);
   if (req.user) {
     next();
   } else {
@@ -117,9 +60,9 @@ app.post('/login',
       res.redirect('/secret');
     });
 
-app.get('/logout', (req, res) => {
+app.get('/logout', function(req, res){
   req.logout();
-  res.redirect('/');
+  res.redirect('/form');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
